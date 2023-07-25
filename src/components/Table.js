33940555a82930d345b6f2e33b91349026523f66
zahid-hossain34 <zahid07.loox@gmail.com>
2023-07-25 16:11:10 +0600

    some changes

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,7 +7,7 @@ function Table({ socialtableImgSrc, children }) {
     <section className="mt-[100px]">
       <div className="container flex flex-col justify-center items-center gap-10">
         <div>{children}</div>
-        <div className="w-[1590px] overflow-x-hidden h-[497px]  ">
+        <div className="w-[1590px] overflow-x-hidden h-[497px]">
           <img className="w-full h-full" src={imgSrc} alt="" />
         </div>
       </div>
